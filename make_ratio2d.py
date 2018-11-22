3d2f057666f5206082ef6622b994200b36b5fa5f
Freya Blekman <freya.blekman@cern.ch>
2018-11-22 13:54:27 +0100

    version used for FTR-18-031

--- make_ratio2d.py
+++ make_ratio2d.py
@@ -6,7 +6,7 @@ import CMS_lumi, tdrstyle
 import itertools
 
 
-
+do1d=1
 
 tdrstyle.setTDRStyle()
 
@@ -17,10 +17,10 @@ CMS_lumi.extraText = "Simulation Preliminary"
 outfile = rt.TFile.Open("out.root","RECREATE")
 outfile.cd()
 wilsoncoeff_names=["C_{O_{R}}","C_{O_{L1}}","C_{O_{L8}}","C_{B1}","C_{B8}"]
-thresholdval0=1.0
-thresholdval1=0.30
-thresholdval2=0.09
-thresholdval3=0.01
+thresholdval0=[1.0]
+thresholdval1=[0.30,(0.43+0.39)/2.]
+thresholdval2=[0.09,(0.28+0.24)/2.]
+thresholdval3=[0.01]
 
 
 number_of_points_in_direction = [100,100]
@@ -38,46 +38,52 @@ eft14 = EftPredictions(wilson_coefficients, MG_SM_14TeV, sig_SM_14TeV)
 eft27 = EftPredictions(wilson_coefficients, MG_SM_27TeV, sig_SM_27TeV)
 
 # 1D first
-
-for iwilson in range(0,5):
-    number_of_points_in_direction = 10000
-    max_range =  4.0*np.pi
-    min_range = -4.0*np.pi
-    step_size =  (max_range - min_range)/number_of_points_in_direction
-    histo1d13 = rt.TGraph(number_of_points_in_direction+1)
-    histo1d13.SetName("13 TeV")
-    histo1d14 = rt.TGraph(number_of_points_in_direction+1)
-    histo1d14.SetName("14 TeV")
-    histo1d27 = rt.TGraph(number_of_points_in_direction+1)
-    histo1d27.SetName("27 TeV")
-    for i in range(0,number_of_points_in_direction+1):
-        x = min_range+i*step_size
-        workarrayhorizontal=[0,0,0,0,0]
-        workarrayhorizontal[iwilson]=x
-        histo1d13.SetPoint(i,x,eft13.gen_eft_xs(workarrayhorizontal)/eft13.gen_eft_xs([0,0,0.,0.,0.]))
-        histo1d14.SetPoint(i,x,eft14.gen_eft_xs(workarrayhorizontal)/eft14.gen_eft_xs([0,0,0.,0.,0.]))
-        histo1d27.SetPoint(i,x,eft27.gen_eft_xs(workarrayhorizontal)/eft27.gen_eft_xs([0,0,0.,0.,0.]))
-
-    canv = rt.TCanvas()
-    histo1d13.Draw("la")
-    histo1d14.Draw("l")
-    histo1d27.Draw("l")
-    canv.Update()
-#    histo1d13.Fit(myfunc,"Q","goff")
+if do1d :
+    for iwilson in range(0,5):
+        if iwilson == 2:
+            continue
+        number_of_points_in_direction = 10000
+        max_range =  4.0*np.pi
+        min_range = -4.0*np.pi
+        step_size =  (max_range - min_range)/number_of_points_in_direction
+        histo1d13 = rt.TGraph(number_of_points_in_direction+1)
+        histo1d13.SetName("13 TeV")
+        histo1d14 = rt.TGraph(number_of_points_in_direction+1)
+        histo1d14.SetName("14 TeV")
+        histo1d27 = rt.TGraph(number_of_points_in_direction+1)
+        histo1d27.SetName("27 TeV")
+        for i in range(0,number_of_points_in_direction+1):
+            x = min_range+i*step_size
+            workarrayhorizontal=[0,0,0,0,0]
+            workarrayhorizontal[iwilson]=x
+            histo1d13.SetPoint(i,x,eft13.gen_eft_xs(workarrayhorizontal)/eft13.gen_eft_xs([0,0,0.,0.,0.]))
+            histo1d14.SetPoint(i,x,eft14.gen_eft_xs(workarrayhorizontal)/eft14.gen_eft_xs([0,0,0.,0.,0.]))
+            histo1d27.SetPoint(i,x,eft27.gen_eft_xs(workarrayhorizontal)/eft27.gen_eft_xs([0,0,0.,0.,0.]))
+        
+        canv = rt.TCanvas()
+        histo1d13.Draw("la")
+        histo1d14.Draw("l")
+        histo1d27.Draw("l")
+        canv.Update()
+        #    histo1d13.Fit(myfunc,"Q","goff")
 
     for histowork in histo1d13,histo1d14,histo1d27:
         histowork.Fit(myfunc,"Q","goff")
-        for workval in thresholdval0,thresholdval1,thresholdval2,thresholdval3:
+        for workval in thresholdval0[0],thresholdval1[0],thresholdval1[1],thresholdval2[0],thresholdval2[1],thresholdval3[0]:
             a2=myfunc.GetParameter(0)
             b2=myfunc.GetParameter(1)
             c2=myfunc.GetParameter(2)-workval-1
             det2 =rt.TMath.Sqrt((b2*b2)-(4*a2*c2))
-            print wilsoncoeff_names[iwilson], histowork.GetName(), workval, round((-b2+det2)/(2*a2),2), round((-b2-det2)/(2*a2),2)
-#, round(histowork.Eval((-b2+det2)/(2*a2)),2),round(histowork.Eval((-b2-det2)/(2*a2)),2)
+            print " $ ",wilsoncoeff_names[iwilson]," $ &", histowork.GetName()," & ", workval," & ", round((-b2+det2)/(2*a2),2)," & ", round((-b2-det2)/(2*a2),2)," \\\\ "
+    #, round(histowork.Eval((-b2+det2)/(2*a2)),2),round(histowork.Eval((-b2-det2)/(2*a2)),2)
 
 for combo in itertools.combinations(range(0,5),2) :
     print combo[0],combo[1],wilsoncoeff_names[combo[0]],wilsoncoeff_names[combo[1]]
 
+    if combo[0]==2 :
+        continue
+    if combo[1]==2 :
+        continue
     # do a simple loop over the diagonal to determine range first
 
     number_of_points_in_direction = [100,100]
@@ -110,13 +116,13 @@ for combo in itertools.combinations(range(0,5),2) :
 
     a1=myfunc.GetParameter(0)
     b1=myfunc.GetParameter(1)
-    c1=myfunc.GetParameter(2)-thresholdval0-1
+    c1=myfunc.GetParameter(2)-thresholdval0[0]-1
     det1=rt.TMath.Sqrt((b1*b1)-(4*a1*c1))
 
     histo1d2.Fit(myfunc,"Q","goff")
     a2=myfunc.GetParameter(0)
     b2=myfunc.GetParameter(1)
-    c2=myfunc.GetParameter(2)-thresholdval0-1
+    c2=myfunc.GetParameter(2)-thresholdval0[0]-1
     det2 =rt.TMath.Sqrt((b2*b2)-(4*a2*c2))
     # assuming a parabola, analytically solve range points:
     print wilsoncoeff_names[combo[0]], det1, (-b1+det1)/(2*a1), (-b1-det1)/(2*a1), histo1d1.Eval((-b1+det1)/(2*a1)),histo1d1.Eval((-b1-det1)/(2*a1))
@@ -133,7 +139,7 @@ for combo in itertools.combinations(range(0,5),2) :
         workrange[1]=0.1
 
     print "scanned range, setting to ",workrange
-    max_range=[1.2*workrange[0],2*workrange[1]]
+    max_range=[1.2*workrange[0],2.5*workrange[1]]
     min_range=[-1.2*workrange[0],-1.2*workrange[1]]
     number_of_points_in_direction[0]*=10
     number_of_points_in_direction[1]*=10
@@ -141,16 +147,22 @@ for combo in itertools.combinations(range(0,5),2) :
     step_size = [ (max_range[0] - min_range[0])/number_of_points_in_direction[0], (max_range[1] - min_range[1])/number_of_points_in_direction[1] ]
 #    print max_range,min_range,step_size
     
-    hist2d = rt.TH2D('hist2d_'+str(combo[0])+str(combo[1]),'fractional difference 13 TeV;'+wilsoncoeff_names[combo[0]]+';'+wilsoncoeff_names[combo[1]],number_of_points_in_direction[0],min_range[0]-step_size[0]/2.,max_range[0]+step_size[0]/2.,
+    hist2d = rt.TH2D('hist2d_'+str(combo[0])+str(combo[1]),'fractional difference 13 TeV;'+wilsoncoeff_names[combo[0]]+' [GeV^{-2}];'+wilsoncoeff_names[combo[1]]+' [GeV^{-2}]',number_of_points_in_direction[0],min_range[0]-step_size[0]/2.,max_range[0]+step_size[0]/2.,
                  number_of_points_in_direction[1],min_range[1]-step_size[1]/2.,max_range[1]+step_size[1]/2.)
 
     hist2d.SetTitle("")
     scenario0=hist2d.Clone("scenario0_"+str(combo[0])+str(combo[1]))
+    scenario0a=hist2d.Clone("scenario0a_"+str(combo[0])+str(combo[1]))
+    
     scenario1=hist2d.Clone("scenario1_"+str(combo[0])+str(combo[1]))
+    scenario1a=hist2d.Clone("scenario1a_"+str(combo[0])+str(combo[1]))
+
     scenario2=hist2d.Clone("scenario2_"+str(combo[0])+str(combo[1]))
+    scenario2a=hist2d.Clone("scenario2a_"+str(combo[0])+str(combo[1]))
+    
     scenario3=hist2d.Clone("scenario3_"+str(combo[0])+str(combo[1]))
-
-
+    
+    print scenario0,scenario1,scenario2,scenario3
 
     for i in range(0,number_of_points_in_direction[0]+1):
         for j in range(0,number_of_points_in_direction[0]+1):
@@ -162,59 +174,78 @@ for combo in itertools.combinations(range(0,5),2) :
             #            print workarray
             #        print i,j,x,y,xs
             hist2d.SetBinContent(i,j,eft13.gen_eft_xs(workarray)/eft13.gen_eft_xs([0,0,0.,0.,0.]))
-            if eft13.gen_eft_xs(workarray)/eft13.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval0 :
+            if eft13.gen_eft_xs(workarray)/eft13.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval0[0] :
                 #            print i,j,x,y,xs
                 scenario0.SetBinContent(i,j,0.)
             else:
                 scenario0.SetBinContent(i,j,1.)
-            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval1 :
+            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval1[0] :
                 #            print i,j,x,y,xs
                 scenario1.SetBinContent(i,j,0.)
             else:
                 scenario1.SetBinContent(i,j,1.)
-            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval2 :
+            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval1[1] :
+                #            print i,j,x,y,xs
+                scenario1a.SetBinContent(i,j,0.)
+            else:
+                scenario1a.SetBinContent(i,j,1.)
+            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval2[0] :
                 #            print i,j,x,y,xs
                 scenario2.SetBinContent(i,j,0.)
             else:
                 scenario2.SetBinContent(i,j,1.)
-            if eft27.gen_eft_xs(workarray)/eft27.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval3 :
+            if eft14.gen_eft_xs(workarray)/eft14.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval2[1] :
+                #            print i,j,x,y,xs
+                scenario2a.SetBinContent(i,j,0.)
+            else:
+                scenario2a.SetBinContent(i,j,1.)
+            if eft27.gen_eft_xs(workarray)/eft27.gen_eft_xs([0,0,0.,0.,0.]) < 1.+thresholdval3[0] :
                 #            print i,j,x,y,xs
                 scenario3.SetBinContent(i,j,0.)
             else:
                 scenario3.SetBinContent(i,j,1.)
 
-    legendstring0 = "LHC 13TeV 35.9 fb^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval0,0))+"%"
-    legendstring1 = "HL-LHC 3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval1,0))+"%"
-    legendstring2 = "HL-LHC 0.3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval2,0))+"%"
-    legendstring3 = "HE-LHC 15 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval3,0))+"%"
+    legendstring0 = "LHC 13TeV, #Delta#sigma_{tttt} : "+str(round(100*thresholdval0[0],0))+"% (reference)"
+    legendstring1 = "HL-LHC 0.3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval1[0],0))+"% (stat)"
+    legendstring1a = "HL-LHC 0.3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval1[1],0))+"% (stat+syst)"
+    legendstring2 = "HL-LHC 3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval2[0],0))+"% (stat)"
+    legendstring2a = "HL-LHC 3 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval2[1],0))+"% (stat+syst)"
+    legendstring3 = "HE-LHC 15 ab^{-1}, #Delta#sigma_{tttt} : "+str(round(100*thresholdval3[0],0))+"% (stat)"
 
     hist2d.SetTitle("")
     #hist2d.Draw("CONT1Z")
     #rt.gPad.SetLogz()
     hist2d.Draw("axis")
-    scenario0.SetLineColor(rt.kGrey)
-    scenario0.SetLineStyle(3)
+    scenario0.SetLineColor(rt.kGray)
     scenario0.Draw("CONT3SAME")
     scenario1.SetLineColor(rt.kAzure+1)
     scenario1.Draw("CONT3SAME")
+    scenario1a.SetLineColor(rt.kAzure+0)
+    scenario1a.Draw("CONT3SAME")
     scenario2.SetLineColor(rt.kGreen+3)
     scenario2.Draw("CONT3SAME")
+    scenario2a.SetLineColor(rt.kGreen+2)
+    scenario2a.Draw("CONT3SAME")
     scenario3.SetLineColor(rt.kRed)
     scenario3.Draw("CONT3SAME")
     rt.gPad.Update()
-    leg = rt.TLegend(0.5,0.75,0.93,0.93)
+    leg = rt.TLegend(0.49,0.61,0.96,0.93)
     leg.SetFillStyle(0)
     leg.SetBorderSize(0)
     leg.AddEntry(scenario0,legendstring0,"l")
     leg.AddEntry(scenario1,legendstring1,"l")
+    leg.AddEntry(scenario1a,legendstring1a,"l")
     leg.AddEntry(scenario2,legendstring2,"l")
+    leg.AddEntry(scenario2a,legendstring2a,"l")
     leg.AddEntry(scenario3,legendstring3,"l")
+    leg.SetEntrySeparation(0.5*leg.GetEntrySeparation())
+    leg.SetMargin(0.5*leg.GetMargin())
     leg.Draw("same")
     CMS_lumi.CMS_lumi(rt.gPad,0,11)
     rt.gPad.Update()
     rt.gPad.Print("plot"+str(combo[0])+str(combo[1])+".png")
     rt.gPad.Print("plot"+str(combo[0])+str(combo[1])+".pdf")
-    rt.gPad.Print("plot"+str(combo[0])+str(combo[1])+".C")
+    rt.gPad.Print("plot"+str(combo[0])+str(combo[1])+".root")
     outfile.cd()
 hist2d.Write()
 outfile.Close()
